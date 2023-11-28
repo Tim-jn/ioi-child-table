38d0816b4c98cfbebe48ec78ff12e69712f0fbc3
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-11-28 18:48:45 +0100

    feat: Add before_print hook to format headings for item builder

--- construction/hooks.py
+++ construction/hooks.py
@@ -29,9 +29,9 @@ app_include_js = "construction.bundle.js"
 
 # include js in doctype views
 doctype_js = {
-	"Quotation" : "public/js/quotation.js",
-	"Project" : "public/js/project.js",
-    "Task" : "public/js/task.js"
+	"Quotation": "public/js/quotation.js",
+	"Project": "public/js/project.js",
+	"Task": "public/js/task.js",
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
@@ -50,7 +50,7 @@ doctype_js = {
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# 	"Role": "home_page"
 # }
 
 # Generators
@@ -64,8 +64,8 @@ doctype_js = {
 
 # add methods and filters to jinja environment
 # jinja = {
-#	"methods": "construction.utils.jinja_methods",
-#	"filters": "construction.utils.jinja_filters"
+# 	"methods": "construction.utils.jinja_methods",
+# 	"filters": "construction.utils.jinja_filters"
 # }
 
 # Installation
@@ -108,11 +108,11 @@ after_migrate = "construction.install.after_migrate"
 # Permissions evaluated in scripted ways
 
 # permission_query_conditions = {
-#	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
 # }
 #
 # has_permission = {
-#	"Event": "frappe.desk.doctype.event.event.has_permission",
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
 # }
 
 # DocType Class
@@ -120,40 +120,38 @@ after_migrate = "construction.install.after_migrate"
 # Override standard doctype classes
 
 # override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
+# 	"ToDo": "custom_app.overrides.CustomToDo"
 # }
 
 # Document Events
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
-# }
+doc_events = {
+	"Quotation": {
+		"before_print": "construction.construction.print.before_print",
+	}
+}
 
 # Scheduled Tasks
 # ---------------
 
 # scheduler_events = {
-#	"all": [
-#		"construction.tasks.all"
-#	],
-#	"daily": [
-#		"construction.tasks.daily"
-#	],
-#	"hourly": [
-#		"construction.tasks.hourly"
-#	],
-#	"weekly": [
-#		"construction.tasks.weekly"
-#	],
-#	"monthly": [
-#		"construction.tasks.monthly"
-#	],
+# 	"all": [
+# 		"construction.tasks.all"
+# 	],
+# 	"daily": [
+# 		"construction.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"construction.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"construction.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"construction.tasks.monthly"
+# 	],
 # }
 
 # Testing
@@ -165,14 +163,14 @@ after_migrate = "construction.install.after_migrate"
 # ------------------------------
 #
 # override_whitelisted_methods = {
-#	"frappe.desk.doctype.event.event.get_events": "construction.event.get_events"
+# 	"frappe.desk.doctype.event.event.get_events": "construction.event.get_events"
 # }
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
 # override_doctype_dashboards = {
-#	"Task": "construction.task.get_dashboard_data"
+# 	"Task": "construction.task.get_dashboard_data"
 # }
 
 # exempt linked doctypes from being automatically cancelled
@@ -197,29 +195,29 @@ after_migrate = "construction.install.after_migrate"
 # --------------------
 
 # user_data_fields = [
-#	{
-#		"doctype": "{doctype_1}",
-#		"filter_by": "{filter_by}",
-#		"redact_fields": ["{field_1}", "{field_2}"],
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_2}",
-#		"filter_by": "{filter_by}",
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_3}",
-#		"strict": False,
-#	},
-#	{
-#		"doctype": "{doctype_4}"
-#	}
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
 # ]
 
 # Authentication and authorization
 # --------------------------------
 
 # auth_hooks = [
-#	"construction.auth.validate"
+# 	"construction.auth.validate"
 # ]
