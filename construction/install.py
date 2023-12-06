f34eaee2265b17a1b2fd09a03aae9d9491c442de
Charles-Henri Decultot <chdecultot@dokos.io>
2023-12-06 07:32:41 +0100

    feat: Hide update stock button for progress invoices

--- construction/install.py
+++ construction/install.py
@@ -1,6 +1,7 @@
 import click
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from frappe.custom.doctype.property_setter.property_setter import make_property_setter
 
 
 def after_install():
@@ -19,6 +20,7 @@ def add_custom_fields():
 	click.secho("* Adding Construction Custom Fields")
 	custom_fields = get_custom_fields()
 	create_custom_fields(custom_fields)
+	create_property_setters()
 
 
 def get_custom_fields_for_selling_doctype(dt: str):
