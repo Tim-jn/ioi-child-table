5ce7113c8f814d491135a236dee2a7d28c4055b2
Charles-Henri Decultot <chdecultot@dokos.io>
2024-01-05 09:36:09 +0100

    fix: Add section in tasks too

--- construction/install.py
+++ construction/install.py
@@ -156,6 +156,12 @@ def get_custom_fields():
 				"insert_after": "template_task",
 			},
 			{
+				"fieldname": "documents_section",
+				"fieldtype": "Section Break",
+				"insert_after": "documents_tab",
+				"hidden": 1
+			},
+			{
 				"fieldname": "documents_html",
 				"fieldtype": "HTML",
 				"label": "Documents",
