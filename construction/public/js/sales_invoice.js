ee0901f0cba4f7e74368277a9293b4483ed6ddba
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2024-03-25 15:58:56 +0100

    fix(builder): Call destroy on previous instance on form refresh

--- construction/public/js/sales_invoice.js
+++ construction/public/js/sales_invoice.js
@@ -1,8 +1,5 @@
 frappe.ui.form.on("Sales Invoice", {
 	refresh(frm) {
-		frm.item_builder = new construction.item_builder({
-			frm: frm,
-			$wrapper: frm.get_field("item_builder_html").$wrapper,
-		})
+		construction.setup_quotation_builder(frm);
 	},
 })
