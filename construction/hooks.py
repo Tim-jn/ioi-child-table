8e698f38a656a3ab5b3df2ca22b4ae763f723428
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2024-04-11 15:16:48 +0200

    refactor: Move JS doctype overrides

--- construction/hooks.py
+++ construction/hooks.py
@@ -29,11 +29,11 @@ app_include_js = "construction.bundle.js"
 
 # include js in doctype views
 doctype_js = {
-	"Quotation" : "public/js/quotation.js",
-	"Project" : "public/js/project.js",
-	"Task" : "public/js/task.js",
-	"Sales Order" : "public/js/sales_order.js",
-	"Sales Invoice" : "public/js/sales_invoice.js"
+	"Quotation": "public/js/overrides/quotation.js",
+	"Project": "public/js/overrides/project.js",
+	"Task": "public/js/overrides/task.js",
+	"Sales Order": "public/js/overrides/sales_order.js",
+	"Sales Invoice": "public/js/overrides/sales_invoice.js",
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
@@ -142,9 +142,7 @@ doc_events = {
 	"Sales Invoice": {
 		"before_print": "construction.construction.print.before_print",
 	},
-	"Project": {
-		"on_update": "construction.utils.projects.on_update"
-	},
+	"Project": {"on_update": "construction.utils.projects.on_update"},
 }
 
 # Scheduled Tasks
