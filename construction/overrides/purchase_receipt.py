79d9af58f80c288b7fc161e5387e7ca613adb5de
Charles-Henri Decultot <chdecultot@dokos.io>
2024-06-07 11:41:19 +0200

    fix: activate custom logic

--- construction/overrides/purchase_receipt.py
+++ construction/overrides/purchase_receipt.py
@@ -3,7 +3,6 @@ from erpnext.stock.doctype.purchase_receipt.purchase_receipt import PurchaseRece
 from construction.overrides.status_updater import add_row_type_condition_to_status_updater
 
 class ConstructionPurchaseReceipt(PurchaseReceipt):
-	pass
-	# def __init__(self, *args, **kwargs):
-	# 	super().__init__(*args, **kwargs)
-	# 	self.status_updater = add_row_type_condition_to_status_updater(self.status_updater)
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.status_updater = add_row_type_condition_to_status_updater(self.status_updater)
