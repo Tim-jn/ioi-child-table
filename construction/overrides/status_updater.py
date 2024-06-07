e66bb2e3c6cf17e27079dfe1d96651188eb80a00
Charles-Henri Decultot <chdecultot@dokos.io>
2024-06-07 11:49:35 +0200

    fix: Exclude material requests

--- construction/overrides/status_updater.py
+++ construction/overrides/status_updater.py
@@ -1,7 +1,7 @@
 def add_row_type_condition_to_status_updater(status_updater):
 	updated_status_updater = status_updater.copy()
 	for updater in updated_status_updater:
-		if updater.get("target_parent_dt"):
+		if updater.get("target_parent_dt") and updater.get("target_parent_dt") != "Material Request":
 			updater["target_dt_condition"] = "and COALESCE(row_type, '') in ('', 'item')"
 
 	return updated_status_updater
