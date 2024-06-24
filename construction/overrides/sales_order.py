858e24d5db141bce10baa234c0e5715d7fdb7654
Charles-Henri Decultot <chdecultot@dokos.io>
2024-06-24 22:14:16 +0200

    fix: improve progress percentage calculation

--- construction/overrides/sales_order.py
+++ construction/overrides/sales_order.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.utils import flt
 from erpnext.selling.doctype.sales_order.sales_order import make_sales_invoice
 
 @frappe.whitelist()
@@ -6,9 +7,21 @@ def make_progress_invoice(source_name, target_doc=None):
 	doclist = make_sales_invoice(source_name, target_doc=target_doc)
 	doclist.is_progress_invoice = 1
 
-	sales_orders = list(set(item.sales_order for item in doclist.items))
-	already_billed = max(frappe.db.get_value("Sales Order", sales_order, "per_billed") for sales_order in sales_orders)
+	project_advancement = 0.0
+	if doclist.project:
+		project_advancement = flt(frappe.db.get_value("Project", doclist.project, "percent_complete"))
 
-	doclist.progress_percentage = already_billed
+	for item in doclist.items:
+		if not item.so_detail or item.row_type not in ["Item", ""]:
+			continue
+
+		base_net_amount, billed_amt = frappe.db.get_value("Sales Order Item", item.so_detail, ["base_net_amount", "billed_amt"])
+		already_billed = flt(billed_amt) / flt(base_net_amount) * 100.0
+		qty_to_bill = frappe.db.get_value("Sales Order Item", item.so_detail, "qty")
+		item.progress_percentage = max([project_advancement, already_billed])
+		item.qty = (flt(item.progress_percentage) - flt(already_billed)) / 100.0 * flt(qty_to_bill)
+
+	doclist.run_method("calculate_progress")
+	doclist.run_method("calculate_taxes_and_totals")
 
 	return doclist
--- construction/public/js/overrides/sales_order.js
+++ construction/public/js/overrides/sales_order.js
@@ -5,7 +5,7 @@ frappe.ui.form.on("Sales Order", {
 		if(!["Closed", "On Hold"].includes(frm.doc.status) && flt(frm.doc.per_billed, 2) < 100) {
 			frm.add_custom_button(__('Progress Invoice'), () => {
 				frappe.model.open_mapped_doc({
-					method: "construction.override.sales_order.make_progress_invoice",
+					method: "construction.overrides.sales_order.make_progress_invoice",
 					frm: frm
 				})
 			}, __('Create'));
