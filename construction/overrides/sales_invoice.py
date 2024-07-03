e44b0a41b42f10fb92e439b7c2decfb2a94701bd
Charles-Henri Decultot <chdecultot@dokos.io>
2024-07-03 18:57:45 +0200

    fix: remove unused files

--- construction/overrides/sales_invoice.py
+++ construction/overrides/sales_invoice.py
@@ -24,10 +24,19 @@ class ConstructionSalesInvoice(SalesInvoice):
 
 	def validate_uom_is_integer(self, uom_field, qty_fields):
 		if not self.is_progress_invoice:
-			super(ConstructionSalesInvoice, self).validate_uom_is_integer(uom_field, qty_fields)
+			super().validate_uom_is_integer(uom_field, qty_fields)
 
 	def validate(self):
 		super().validate()
+
+		if self.is_progress_invoice:
+			if len(set(item.sales_order for item in self.items)) > 1:
+				frappe.throw(_("A progress invoice can only be linked to a single sales order"))
+
+			if self.update_stock:
+				self.update_stock = 0
+				frappe.msgprint(_("Stock cannot be updated directly from a progress invoice"), alert=True)
+
 		self.calculate_progress()
 
 
