c3afeff8ba765c7c26a60811976b0ee1232b734c
Charles-Henri Decultot <chdecultot@dokos.io>
2024-09-25 17:55:38 +0200

    fix: progress calculation and adding comments

--- construction/overrides/sales_invoice.py
+++ construction/overrides/sales_invoice.py
@@ -8,6 +8,9 @@ from erpnext.controllers.accounts_controller import validate_account_head
 from construction.overrides.status_updater import add_row_type_condition_to_status_updater
 from construction.construction.doctype.progress_invoicing_items.progress_invoicing_items import set_invoicing_summary
 
+class InvalidProgressCalculationMethodError(frappe.ValidationError):
+	pass
+
 class ConstructionSalesInvoice(SalesInvoice):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
@@ -27,11 +30,12 @@ class ConstructionSalesInvoice(SalesInvoice):
 			super().validate_uom_is_integer(uom_field, qty_fields)
 
 	def validate(self):
+		self.check_sales_invoice_integrity()
 		self.get_sales_order_details()
 		super().validate()
 
 		if self.is_progress_invoice:
-			if len(set(item.sales_order for item in self.items)) > 1:
+			if len(set(item.sales_order for item in self.items if item.sales_order)) > 1:
 				frappe.throw(_("A progress invoice can only be linked to a single sales order"))
 
 			if self.update_stock:
@@ -41,6 +45,18 @@ class ConstructionSalesInvoice(SalesInvoice):
 			self.progress_invoice_no = 0
 
 		self.calculate_progress()
+
+	def check_sales_invoice_integrity(self):
+		if not self.is_progress_invoice or not self.calculate_progress_globally:
+			return
+
+		sales_invoice_rows = [item for item in self.items if item.row_type in ("Item", "") if item.so_detail]
+		sales_order = frappe.get_doc("Sales Order", sales_invoice_rows[0].sales_order)
+		sales_order_rows = [item.name for item in sales_order.items if item.row_type in ("Item", "")]
+		sales_invoice_rows_details = [item.so_detail for item in sales_invoice_rows]
+
+		if set(sales_order_rows).difference(set(sales_invoice_rows_details)):
+			frappe.throw(_("This invoice doesn't match exactly the sales order. Please calculate the progress at line item level."), exc=InvalidProgressCalculationMethodError)
 
 	def get_sales_order_details(self):
 		if not self.is_progress_invoice:
@@ -65,10 +81,12 @@ class ConstructionSalesInvoice(SalesInvoice):
 		if not self.is_progress_invoice:
 			return
 
-		items = [item for item in self.items if item.row_type in ("Item", "")]
+		items = [item for item in self.items if item.row_type in ("Item", "") and item.sales_order]
+		if not items:
+			return
 
 		total_billed = sum(item.sales_order_billed_amount for item in items)
-		if sum_so_amount := sum(item.sales_order_amount for item in items):
+		if sum_so_amount := frappe.db.get_value("Sales Order", items[0].sales_order, "net_total"):
 			self.progress_percentage = (flt(self.base_net_total) + total_billed) / sum_so_amount * 100.0
 
 	def set_print_heading(self):
