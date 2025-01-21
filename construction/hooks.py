04a3018a4329cc26353a826c561df70494ee9559
Charles-Henri Decultot <chdecultot@dokos.io>
2025-01-21 14:34:57 +0100

    feat: Calculate totals in sections

--- construction/hooks.py
+++ construction/hooks.py
@@ -142,19 +142,27 @@ override_doctype_class = {
 doc_events = {
 	"Quotation": {
 		"before_print": "construction.construction.print.before_print",
+		"validate": "construction.overrides.transaction.calculate_section_total"
 	},
 	"Sales Order": {
 		"before_print": "construction.construction.print.before_print",
-		"validate": "construction.construction.doctype.progress_invoicing_items.progress_invoicing_items.set_invoicing_summary",
+		"validate": [
+			"construction.overrides.transaction.calculate_section_total",
+			"construction.construction.doctype.progress_invoicing_items.progress_invoicing_items.set_invoicing_summary"
+		],
 		"on_update_after_submit": "construction.construction.doctype.progress_invoicing_items.progress_invoicing_items.set_invoicing_summary"
 	},
 	"Sales Invoice": {
 		"before_print": "construction.construction.print.before_print",
-		"validate": "construction.construction.doctype.progress_invoicing_items.progress_invoicing_items.set_invoicing_summary",
+		"validate": [
+			"construction.overrides.transaction.calculate_section_total",
+			"construction.construction.doctype.progress_invoicing_items.progress_invoicing_items.set_invoicing_summary"
+		],
 		"on_update_after_submit": "construction.construction.doctype.progress_invoicing_items.progress_invoicing_items.set_invoicing_summary"
 	},
 	"Delivery Note": {
 		"before_print": "construction.construction.print.before_print",
+		"validate": "construction.overrides.transaction.calculate_section_total",
 	},
 	"Purchase Order": {
 		"before_print": "construction.construction.print.before_print",
